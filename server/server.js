32faec392d5dcfa2c430ddd8a1a080784fbd264c
MCKIT77 <jakemckit@gmail.com>
2024-10-14 05:42:02 -0400

    pulled from project repo , group project no longer worked on

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const { Storage } = require('@google-cloud/storage'); // Import the Google Cloud
 const { uploadMiddleware } = require('./utils/upload');
 const multer = require('multer'); // Import multer for file uploads
 const cors = require('cors');
+
 require('dotenv').config();
 
 
@@ -37,19 +38,19 @@ const startApolloServer = async () => {
 
   const storage = new Storage({
     credentials: {
-     "type": "service_account",
-     "project_id": "proxy-199321",
-     "private_key_id": "af9ee72803c7eb1c012a21f332da3e1e423533b6",
-     "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCbAABhB9Xba4QQ\nkH0zbtNk5U7FBFJj7hg4iRErHUXCfMn7YmUpWQXK6dDDcWqB6ZCT0+Uh8+XcwC9Q\nFAFZXUzsQmnqDb7zSDU7bfTV9SxvT7r6c/t3MqMOejjkoVU9vE38afPHhlwbmDla\n3UQjqulnJL56XX/iCpjuQ8m1g43ijOggV7BN0DiHtARdKPZZrf8F5DDsQdAvCcfn\n/ypMMFXaAEtd33oTJuE+tsZeCgiEwaylh1JDNfnTRUZ1I/seelJqDo+qFXF9d/Ay\nzr0DQuMvrn+wPUWQzAH5zwT67ZmJ9RESryGYBVwD6YBD+3/lOv95fqNpMj31X+v6\ni6KvJ7/VAgMBAAECggEAMV73v/Bp+yEtCMWtS5OyfAHabEDrKW9ugZuPP2dsj/2F\nrK/zvw7fHHoLiCV5InZOGfg1lPIGNqF0sJIet+NTHDIvYGEOgJlwc0Sb5wbYJ4CC\n80G8SIR1kaf2tUR6TVbMlGj/QH+YmSWMp1exW5cWD0eR8JIVqN9tRP+MoWeM6yst\ndEhGN3+UiNmTvF2lm8fKZCqytFs6zr+AXYZiAyPlkQ8PuZnxpLbubY9qELbYUcrw\nStJp1kxo+xW/k9anK+HYbg530hOTGig/T4rnQA1NgEkednfV79qwWyAMOdqGey6C\ndfKQHz+nRMuAOzopmKeiXqj26zhIQT+lUYTYFVG35QKBgQDYTs6rf7otRHCeluUI\nwaO0fY3yTfpln5fkHARToiVZCzNqAurrwyHDUyTGI0jfbimQBweronCVEPOj/pi0\nM/jomwcD/GMDxPEPxkxWMA8WFzx6k33Zim9P4XVWMza59OdJKPIJmCsgzZzH9JOt\nz0muIDt64qjaX/pQssyAJm3tnwKBgQC3cTijSeaPNmKgHM3Ro+TirHnrrgOJMdvI\nlXWja8hKQ7NG3Tv2jmjUVVUa5qcZpypEToHyxoPXht5vibWDJcyYypEbQ8wLeBpA\n2qhC9HRATbj6wTWjCud3cPLvX4Cdm7zPQNVjB5ftudpQTpdhbGW1yG03BMli4Mgc\nsdX/t1E2CwKBgG71LgrHQGeMHgEcAmzVd/ZUGYZb/cA3d6DpxkZofKQKnNsamAtM\nU3twoQKKgw0p42QgVwzyDLJKudA7/3Ug2+Pl8llXFCuQ0N94pxvnFzYLwVhXZE09\nOBBP5PLM3pvRRvs+Z7oINsdi8bOYgFC+bltF6X0F2/v5E9xle4WZMPadAoGBAKR6\nhjgS6QF72A6MBRd1hqKf12m0S0NWSyScri/rUlkUPsjJURyPL/tCz94g9a8Qrds1\nMVsHF+vF5+fGZ9PBrqNI7bauoWOfOprv83KlUR7fEX70gi1Ad64mvgxXazDmFPMX\nxLzYcCcdP1xGO4GE+eV2Yka0qgVYWtUvtxtjuRNlAoGBAMX/s2q9j9yWiwtEJLa/\n/R6XbMf01blM2vSaqsFfxng5kNzbYT1c5giRETyGXERPYkR9h93p02qXLP0PaRNC\nSk/LpvOgiHTdw8IHLluciq185V5pWzUHMVnpho0l7/GKCZTYOBqmjvHRs4cgURyl\nfQruTyq7S12vmlgkeFj/fEvp\n-----END PRIVATE KEY-----\n",
-     "client_email": "mckit77@proxy-199321.iam.gserviceaccount.com",
-     "client_id": "117306848315221002731",
-     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-     "token_uri": "https://oauth2.googleapis.com/token",
-     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/mckit77%40proxy-199321.iam.gserviceaccount.com",
-     "universe_domain": "googleapis.com"
-   }
-   });
+      "type": "service_account",
+      "project_id": `${process.env.PROJECT_ID}`,
+      "private_key_id": `${process.env.PRIVATE_KEY_ID}`,
+      "private_key": `${process.env.PRIVATE_KEY}`,
+      "client_email": `${process.env.CLIENT_EMAIL}`,
+      "client_id": `${process.env.CLIENT_ID}`,
+      "auth_uri": `${process.env.AUTH_URL}`,
+      "token_uri": `${process.env.TOKEN_URL}`,
+      "auth_provider_x509_cert_url": `${process.env.AUTH_PROVIDER_X509_CERT_URL}`,
+      "client_x509_cert_url": `${process.env.CLIENT_X509_CERT_URL}`,
+      "universe_domain": "googleapis.com"
+    }
+  });
 
   const bucketName = 'uploads_bucket_instaclip'; // Replace with your GCS bucket name // Replace with your GCS bucket name
   const bucket = storage.bucket(bucketName);
