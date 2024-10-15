d7f090ccbcd1fc9708f56c8906a0c20e130b1761
MCKIT77 <jakemckit@gmail.com>
2024-10-15 17:43:27 -0400

    fixed vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,16 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: process.env.PORT || 3000,
     open: true,
-    proxy: {
+    proxy: process.env.NODE_ENV === 'development' ? {
       '/graphql': {
         target: 'http://localhost:3001',
         secure: false,
         changeOrigin: true
       }
-    }
-  },
+    } : undefined // No proxy in production
+  }
+  ,
   test: {
     globals: true,
     environment: 'happy-dom'
